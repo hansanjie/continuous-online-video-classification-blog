956c6dccf117b0267bdd08bae96f70fa1efb7b50
hansanjie <249501287@qq.com>
2018-07-31 10:32:39 +0800

    改变模型文件地址
    ①改变模型文件地址
    ②改变模型入口地址
    3增加OSC传输

--- online.py
+++ online.py
@@ -8,30 +8,47 @@ from picamera import PiCamera
 from picamera.array import PiRGBArray
 import tensorflow as tf
 
+from pythonosc import osc_message_builder
+from pythonosc import udp_client
+
+
+
 def get_labels():
     """Get a list of labels so we can see if it's an ad or not."""
-    with open('../inception/retrained_labels.txt', 'r') as fin:
+    with open('retrained_labels.txt', 'r') as fin:
         labels = [line.rstrip('\n') for line in fin]
+        #print(labels)
     return labels
 
 def run_classification(labels):
     """Stream images off the camera and process them."""
-
+    
+    sendToAddress = "192.168.0.20"
+    sendToPort = 7000
+    oscName = "/AAAA"
+    
+    client = udp_client.SimpleUDPClient(sendToAddress,sendToPort)
+    
+    
+    
+    
+    
     camera = PiCamera()
-    camera.resolution = (320, 240)
-    camera.framerate = 2
-    raw_capture = PiRGBArray(camera, size=(320, 240))
+    camera.resolution = (224, 224)
+    camera.framerate = 5
+    raw_capture = PiRGBArray(camera, size=(224, 224))
 
     # Warmup...
     time.sleep(2)
 
     # Unpersists graph from file
-    with tf.gfile.FastGFile("../inception/retrained_graph.pb", 'rb') as fin:
+    with tf.gfile.FastGFile('retrained_graph.pb', 'rb') as fin:
         graph_def = tf.GraphDef()
         graph_def.ParseFromString(fin.read())
         _ = tf.import_graph_def(graph_def, name='')
 
     with tf.Session() as sess:
+        
         # And capture continuously forever.
         softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
         for _, image in enumerate(
@@ -40,11 +57,11 @@ def run_classification(labels):
                 )
             ):
             # Get the numpy version of the image.
-            decoded_image = image.array
+            decoded_image = image.array.reshape(1,224,224,3)
 
             # Make the prediction. Big thanks to this SO answer:
             # http://stackoverflow.com/questions/34484148/feeding-image-data-in-tensorflow-for-transfer-learning
-            predictions = sess.run(softmax_tensor, {'DecodeJpeg:0': decoded_image})
+            predictions = sess.run(softmax_tensor, {'Placeholder:0': decoded_image})
             prediction = predictions[0]
 
             # Get the highest confidence category.
@@ -54,7 +71,9 @@ def run_classification(labels):
             predicted_label = labels[max_index]
 
             print("%s (%.2f%%)" % (predicted_label, max_value * 100))
-
+            
+            message = [max_index,predicted_label,max_value]
+            client.send_message(oscName,message)
             # Reset the buffer so we're ready for the next one.
             raw_capture.truncate(0)
 
---
+++ online_for_windows.py
@@ -0,0 +1,87 @@
+"""
+Run the online classification system.
+
+Capture an image, classify, do it again.
+"""
+import time
+from picamera import PiCamera
+from picamera.array import PiRGBArray
+import tensorflow as tf
+
+from pythonosc import osc_message_builder
+from pythonosc import udp_client
+
+import cv2
+
+
+
+def get_labels():
+    """Get a list of labels so we can see if it's an ad or not."""
+    with open('retrained_labels.txt', 'r') as fin:
+        labels = [line.rstrip('\n') for line in fin]
+        #print(labels)
+    return labels
+
+def run_classification(labels):
+    """Stream images off the camera and process them."""
+    
+    sendToAddress = "192.168.0.20"
+    sendToPort = 7000
+    oscName = "/AAAA"
+    
+    client = udp_client.SimpleUDPClient(sendToAddress,sendToPort)
+    
+    
+    
+    
+    
+    camera = PiCamera()
+    camera.resolution = (224, 224)
+    camera.framerate = 5
+    raw_capture = PiRGBArray(camera, size=(224, 224))
+
+    # Warmup...
+    time.sleep(2)
+
+    # Unpersists graph from file
+    with tf.gfile.FastGFile('retrained_graph.pb', 'rb') as fin:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(fin.read())
+        _ = tf.import_graph_def(graph_def, name='')
+
+    with tf.Session() as sess:
+        
+        # And capture continuously forever.
+        softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+        for _, image in enumerate(
+                camera.capture_continuous(
+                    raw_capture, format='bgr', use_video_port=True
+                )
+            ):
+            # Get the numpy version of the image.
+            decoded_image = image.array.reshape(1,224,224,3)
+
+            cv2.imshow("Frame",decoded_image)
+            key = cv2.waitKey(1) && 0xFF
+            # Make the prediction. Big thanks to this SO answer:
+            # http://stackoverflow.com/questions/34484148/feeding-image-data-in-tensorflow-for-transfer-learning
+            predictions = sess.run(softmax_tensor, {'Placeholder:0': decoded_image})
+            prediction = predictions[0]
+
+            # Get the highest confidence category.
+            prediction = prediction.tolist()
+            max_value = max(prediction)
+            max_index = prediction.index(max_value)
+            predicted_label = labels[max_index]
+
+            print("%s (%.2f%%)" % (predicted_label, max_value * 100))
+            
+            message = [max_index,predicted_label,max_value]
+            client.send_message(oscName,message)
+            # Reset the buffer so we're ready for the next one.
+            raw_capture.truncate(0)
+            if key ==ord("q"):
+                break
+
+if __name__ == '__main__':
+    run_classification(get_labels())
